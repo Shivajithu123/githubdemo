541287968397cbb72e1f06a98d3e3684591d9480
Shivadath <jithushivabro8086@gmail.com>
2025-10-23 12:28:32 +0530

    jenkins trigger2

--- src/main/java/com/mycompany/githubdemo/GithubDemo.java
+++ src/main/java/com/mycompany/githubdemo/GithubDemo.java
@@ -9,7 +9,8 @@ package com.mycompany.githubdemo;
  * @author HP
  */
 public class GithubDemo {
-
+public void add(){
+System.out.println(20);}
     public static void main(String[] args) {
         System.out.println("Hello shiva changhed again!");
     }
